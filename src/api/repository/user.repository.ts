4117ffb8a152b5dfc2213882ee5743ab7bc9b1d2
Mateus Camargo Rodrigues <mateus-mac@Mateuss-MacBook-Pro.local>
2022-09-24 17:40:52 -0300

    Creating userController

---
+++ src/api/repository/user.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { UserModel } from "../model/user.model";
+import { User, UserDocument } from "../schemas/user.schema";
+
+@Injectable()
+export class UserRepository {
+
+    constructor(@InjectModel(User.name) private readonly userModel: Model<UserDocument>){}
+
+
+    async create(newUser: UserModel): Promise<any> {
+        return this.userModel.create(newUser);
+    }
+}
